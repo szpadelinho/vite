d28b498939ccf93776ef2f73a460d21ece95e51a
Filip <xxxxxxxxxxxxxxxxxxxxxxx@mail.com>
2023-11-21 16:01:31 +0100

    Update new project files

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,60 @@ import ProductForm from './ProductForm.jsx'
 import Chopi from './Chopi.jsx'
 import Child from './Child.jsx'
 
+const Student = ({student}) =>{
+  console.log(student)
+  return(
+    <div>
+      <h2>Imię: {student.name}</h2>
+      <p>Klasa: {student.grade}</p>
+    </div>
+  )
+}
+
+const StudentList = () => {
+  const students = [
+    {
+      id: 1,
+      name: "Zygfryd",
+      grade: 4
+    },
+    {
+      id: 2,
+      name: "Paris",
+      grade: 5
+    },
+    {
+      id: 3,
+      name: "Gniewomir",
+      grade: 8
+    },
+    {
+      id: 4,
+      name: "Szymonek",
+      grade: 1
+    }
+  ]
+  return(
+    <div>
+      {students.map(uczen => (<Student key={uczen.id} student={uczen}/>))}
+    </div>
+  )
+}
+
 function App(){
-  const [dataFromChild, setDataFromChild] = useState(null);
+  //const [dataFromChild, setDataFromChild] = useState(null);
 
   return(
     <>
       {/* <Chopi name="Biegnący z bizonami" age="44"/>
       <Chopi name="Tańczący z wilkami" age="24"/>
       <Chopi name="Chodzący z kotami" age="34"/> */}
-      <Child childDataHandler={setDataFromChild}/>
+      {/* <Child childDataHandler={setDataFromChild}/>
+      {
+        dataFromChild && <h3>{dataFromChild} !!</h3>
+      } */}
+      {/* <Student student={{name: "Bartek", grade: "IIB"}}/> */}
+      <StudentList/>
     </>
   )
 }
--- src/Child.jsx
+++ src/Child.jsx
@@ -1,17 +1,24 @@
+import {useState} from "react"
+import "./Child.css"
+
 const Child = ({childDataHandler}) => {
 
-    function getDataFromUser(){
+    let [userData, setUserData] = useState("");
 
+    function getDataFromUser(event){
+        console.log(event.target.value)
+        setUserData(event.target.value)
     }
 
     function dataSender(){
-        
+        childDataHandler(userData);
+        userData=""
     }
 
     return(
         <div>
             <h2>Jakie dane chcesz wysłać do rodzica?</h2>
-            <input type="text" value="" onChange={getDataFromUser}/>
+            <input type="text" value={userData} onChange={getDataFromUser}/>
             <button onClick={dataSender}>Prześlij dane</button>
         </div>
     )
